a2bdd9e1e3fede8320a089e55f3d4529cc6dd501
Shawn O'Hare <ohare.shawn@gmail.com>
2015-11-02 19:07:22 -0800

    Add more errors.

--- errors.go
+++ errors.go
@@ -1,9 +1,11 @@
 package multiparse
 
 const (
-	ParseMonetaryStringError = "Cannot parse the string as a monetary type."
+	ParseMonetaryStringError = "Cannot parse string as a monetary type."
 	ParseMoneyError          = ParseMonetaryStringError
 	ParseMoneySeparatorError = "Cannot distinguish digit and decimal separators."
-	ParseNumericError        = "Cannot parse the string as a numeric type."
-	ParseTimeError           = "Cannot parse the string as a time."
+	ParseNumericError        = "Cannot parse string as a numeric type."
+	ParseTimeError           = "Cannot parse string as a time."
+	ParseTypeAssertError     = "Cannot assert correct type for parsed value."
+	ParseError               = "Cannot parse string as any valid type."
 )
