fb9283bd1856564cca067cd669dd932f51d8ec24
Shawn O'Hare <ohare.shawn@gmail.com>
2015-12-07 11:48:59 -0800

    Add more datetime formats.

--- time.go
+++ time.go
@@ -26,9 +26,13 @@ var commonTimeLayouts = []string{
 	time.StampMilli,
 	time.StampMicro,
 	time.StampNano,
+	"1/2/06 15:04", // xls style
+	"2/1/06 15:04", // xls style
 }
 
 var commonDateLayouts = []string{
+	"1/2/06", // xls style
+	"2/1/06", // xls style
 	"01-02-06",
 	"02-01-06", // This may not ever match
 	"1/2/2006",
